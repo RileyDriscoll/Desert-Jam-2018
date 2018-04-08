727784e570612ac2233dd9df6d028c00de8f6262
RileyDriscoll <shiningimpact1@gmail.com>
2018-04-07 21:45:51 -0700

    Im acutally amazing
    changing time baby

---
+++ Desert Game Jam 2018/Assets/Scripts/Fade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Desert Game Jam 2018/Assets/Scripts/Misc.cs
+++ Desert Game Jam 2018/Assets/Scripts/Misc.cs
@@ -9,13 +9,14 @@ public class Misc : MonoBehaviour {
     public string levelname;
     void Awake()
     {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
         Application.targetFrameRate = 60;
     }
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {

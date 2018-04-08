9b0ac1f111507ce05479d81c4b349082f5f5291c
RileyDriscoll <shiningimpact1@gmail.com>
2018-04-08 11:09:30 -0700

    Leve Switch added
    neat

---
+++ Desert Game Jam 2018/Assets/SwitchLevels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevels : MonoBehaviour {
+
+    private AssetBundle myLoadedAssetBundle;
+    public string sceneToLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) 
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
